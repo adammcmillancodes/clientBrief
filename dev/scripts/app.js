2ae60ddb047f458e32b9f79e14a77b6332d72491
Adam <adammcmillancodes@gmail.com>
2018-03-21 14:30:34 -0400

    getting results back now

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+axios.get("https://cors-anywhere.herokuapp.com/api.indeed.com/ads/apisearch", {
+  params: {
+    publisher: "6808461958676807",
+    v: 2,
+    format: "json",
+    q: "Front End Web Developer",
+    limit: 25
+  }
+})
+
+  .then(function (res) {
+    console.log(res.data.results);
+  });
 
 class App extends React.Component {
     render() {

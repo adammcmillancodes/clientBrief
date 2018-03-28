c9b60314786b55f3836e1bc36984e9260bc5cc61
Adam <adammcmillancodes@gmail.com>
2018-03-28 15:30:41 -0400

    getting back results based on user input, code review pleeeease

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,76 +2,92 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import JobSearch from './JobSearch';
-// import {
-//   BrowserRouter as Router,
-//   Route, Link
-// } from 'react-router-dom';
 
 
 class App extends React.Component {
-    constructor(){
-      super();
-      this.state = {
-        jobListing: [],
-        jobTitle: '',
-        jobLocation: ''
-      }
-      this.handleChange = this.handleChange.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      jobListing: [],
+      jobTitle: "",
+      jobLocation: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(e){
-     console.log(this)
-      this.setState({
-        [e.target.id]: e.target.value
-      });
-    }
+  handleChange(e) {
+    console.log(this);
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    return this.componentDidMount();
+  }
 
-    componentDidMount(){
-      axios.get("https://cors-anywhere.herokuapp.com/api.indeed.com/ads/apisearch", {
+  componentDidMount() {
+    axios
+      .get("https://cors-anywhere.herokuapp.com/api.indeed.com/ads/apisearch", {
         params: {
           publisher: "6808461958676807",
           v: 2,
           format: "json",
-          q: "Front End Web Developer",
-          l: "Toronto",
+          q: this.state.jobTitle,
+          l: this.state.jobLocation,
           co: "CA",
           sort: "date",
           highlight: 0,
           limit: 25
         }
-      }).then((res) => {
+      })
+      .then(res => {
         console.log(res.data.results);
         this.setState({ jobListing: res.data.results });
-        
       });
-    }
+  }
 
-    render() {
-      return (
-        <div>
-          <header>
-            <h1>Joblify</h1>
-            <h3>Gainfully helpful</h3>
-          </header>
-          <main>
-            <form>
-              I would to work as a(n) 
-              <input onChange={this.handleChange} value={this.state.jobTitle} type="text" title="jobTitle" id="jobTitle" className="jobTitle" placeholder="Desired title"/>
-              living in 
-              <input onChange={this.handleChange} value={this.state.jobLocation} type="text" title="jobLocation" id="jobLocations" className="jobLocation" placeholder="Job Location"/>
-              <button type="submit" className="submit">Submit</button>
-            </form>
-            {this.state.jobListing.map((job, i) => {
-                      return (
-
-                        <JobSearch job={job} />
-                          
-                      )
-                  })}
-          </main>
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Joblify</h1>
+          <h3>Gainfully helpful</h3>
+        </header>
+        <main>
+          <form onSubmit={this.handleSubmit}>
+            I would to work as a(n)
+            <input
+              onChange={this.handleChange}
+              value={this.state.jobTitle}
+              type="text"
+              title="jobTitle"
+              id="jobTitle"
+              className="jobTitle"
+              placeholder="Desired title"
+            />
+            living in
+            <input
+              onChange={this.handleChange}
+              value={this.state.jobLocation}
+              type="text"
+              title="jobLocation"
+              id="jobLocation"
+              className="jobLocation"
+              placeholder="Job Location"
+            />
+            <button type="submit" className="submit">
+              Submit
+            </button>
+          </form>
+          {this.state.jobListing.map((job, i) => {
+            return <JobSearch job={job} />;
+          })}
+        </main>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
